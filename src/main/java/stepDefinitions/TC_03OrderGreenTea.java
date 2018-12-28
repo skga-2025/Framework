2bac7629b2685eddc40f6b28529d32a2f63243d1
skga-2025 <skga.2025@gmail.com>
2018-12-29 00:27:47 +0530

    add

--- src/main/java/stepDefinitions/TC_03OrderGreenTea.java
+++ src/main/java/stepDefinitions/TC_03OrderGreenTea.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
